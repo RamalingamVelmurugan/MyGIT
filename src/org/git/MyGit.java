6ee269466bb79713ead54bcb36c3fbf858863388
RamalingamVelmurugan <velmuruganavr@gmail.com>
2021-10-22 18:26:33 +0530

    New

---
+++ src/org/git/MyGit.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class MyGit {
+	
+	private void first() {
+System.out.println("first");
+	}
+	private void last() {
+		System.out.println("last");
+			}
+	
+	public static void main(String[] args) {
+			MyGit git = new MyGit();
+			git.first();
+			git.last();
+			
+	}
+
+}
